d3d6e33ed0b472868ac1cd323ac4c6975a32c21c
clementfellah <clement.fellah14@gmail.com>
2023-06-07 15:24:15 +0200

    Revert "Revert "Exercice 1,2 et 3""
    This reverts commit 0aac932ec8c819134791c89142f5652b15bbf87a.

---
+++ Exercice1/app.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('btnValid');
+btn.addEventListener('click', function(){
+    const popUp = document.getElementById('popUp');
+    popUp.style.display = 'none';
+})
